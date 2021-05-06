cb848a10a0f9a83879bbe6e62ff967f7ffbf3ac9
Usman <usmanzoor101@gmail.com>
2021-05-07 00:16:48 +0500

    Pull request update

--- Code/app_storefront_training/cartridge/controllers/Newsletter.js
+++ Code/app_storefront_training/cartridge/controllers/Newsletter.js
@@ -3,14 +3,12 @@
 /* Script Modules */
 var app = require("app_storefront_controllers/cartridge/scripts/app");
 var guard = require("app_storefront_controllers/cartridge/scripts/guard");
-var ISML = require("dw/template/ISML");
 var URLUtils = require("dw/web/URLUtils");
+var Resource = require('dw/web/Resource');
 
 function start() {
 	app.getForm("newsletter").clear();
-	app.getView({
-		ContinueURL: URLUtils.https("Newsletter-HandleForm"),
-	}).render("newsletter");
+	app.getView({ContinueURL: URLUtils.https("Newsletter-HandleForm")}).render("newsletter");
 }
 
 function handleForm() {
@@ -21,44 +19,38 @@ function handleForm() {
 		submit: function (formgroup) {
 			var CustomObjectMgr = require("dw/object/CustomObjectMgr");
 			var email = session.forms.newsletter.email.getHtmlValue();
-			var check = CustomObjectMgr.getCustomObject("newsletter-task", email);
-			if (check) {
-				app.getView({
-					ContinueURL: URLUtils.https("newsletter-HandleForm"),
-					error: "Email already Exists",
-				}).render("newsletter");
+			var alreadyExists = CustomObjectMgr.getCustomObject("newsletter-task", email);
+			if (alreadyExists) {
+				app.getView({ContinueURL: URLUtils.https("Newsletter-HandleForm"), error: Resource.msg('email.error', 'error', null)}).render("newsletter");
 			} else {
-				var Transaction = require("dw/system/Transaction");
 				var PromotionMgr = require("dw/campaign/PromotionMgr");
 				var campaign = PromotionMgr.getCampaign("testing-newsletter-task");
 				campaign.getCoupons().toArray().forEach(function (code) {
-						if (code.ID == "newsletter-coupon") {
-							Transaction.wrap(function () {
-								var nextCode = code.getNextCouponCode();
-								if (nextCode) {
-									var obj = CustomObjectMgr.createCustomObject(
-										"newsletter-task",
-										email
-									);
-									obj.custom.couponCode = nextCode;
-									var Email = app.getModel("Email");
-									var Mail = Email.get("helloworld1", email);
-									Mail.setSubject("Coupon Code");
-									Mail.send({
-										Email: email,
-										code: nextCode,
-									});
-									formgroup.copyTo(obj);
-								}else{
-                  var Logger = require('dw/system/Logger');
-                  Logger.warn('no next coupon code for newsletters.');
-                }
+					if (code.ID == "newsletter-coupon") {
+						var Transaction = require('dw/system/Transaction');
+						Transaction.wrap(function () {
+							var nextCode = code.getNextCouponCode();
+							if (!nextCode) {
+								var Logger = require("dw/system/Logger");
+								Logger.warn("no next coupon code for newsletters.");
+								app.getView({key: 'error'}).render('noCouponCode');	
+							}
+							var obj = CustomObjectMgr.createCustomObject("newsletter-task", email);
+							obj.custom.couponCode = nextCode;
+							var Email = app.getModel("Email");
+							var Mail = Email.get("helloworld1", email);
+							Mail.setSubject(Resource.msg('newsletteremail.subject', 'email', null));
+							Mail.send({
+								Email: email,
+								code: nextCode
 							});
-						}
-					});
-				app.getView().render("Helloformresult");
+							formgroup.copyTo(obj);
+							app.getView().render("Helloformresult");
+						});
+					}
+				});
 			}
-		},
+		}
 	});
 }
 
