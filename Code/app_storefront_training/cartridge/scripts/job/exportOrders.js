cc99dd4ab7365e864411eee622e2188f0929684b
Usman <usmanzoor101@gmail.com>
2021-05-07 12:25:54 +0500

    order exporting

--- Code/app_storefront_training/cartridge/scripts/job/exportOrders.js
+++ Code/app_storefront_training/cartridge/scripts/job/exportOrders.js
@@ -3,8 +3,8 @@
 var Logger = require("dw/system/Logger");
 var File = require("dw/io/File");
 var OrderMgr = require("dw/order/OrderMgr");
-var FileWriter = require('dw/io/FileWriter');
-var XMLStreamWriter = require('dw/io/XMLStreamWriter');
+var FileWriter = require("dw/io/FileWriter");
+var XMLStreamWriter = require("dw/io/XMLStreamWriter");
 
 /**
  * Entry point for job.
@@ -13,55 +13,54 @@ var XMLStreamWriter = require('dw/io/XMLStreamWriter');
  */
 
 function execute(param) {
-	var feedDirectory =
-  //add param here
-		File.IMPEX + File.SEPARATOR + "src" + File.SEPARATOR + "test" + File.SEPARATOR;
+	var feedDirectory =	File.IMPEX + File.SEPARATOR + "src" + File.SEPARATOR + "test" + File.SEPARATOR;
 	var feedFileName = "orderExport.xml";
 	var fullPath = feedDirectory + feedFileName;
 
 	if (!File(feedDirectory).isDirectory()) {
-		// check if directory exists.
-		File(feedDirectory).mkdir(); 
+		File(feedDirectory).mkdir();
 	}
 
 	var getOrders = OrderMgr.searchOrders(
-        "status={0}",
-        "creationDate desc",
-        dw.order.Order.ORDER_STATUS_NEW
+		"status={0}",
+		"creationDate desc",
+		dw.order.Order.ORDER_STATUS_NEW
 	);
 
-    var file = new File(fullPath);
-    var fileWriter  = new FileWriter(file, "UTF-8");
-    var xsw  = new XMLStreamWriter(fileWriter);
-    xsw.writeStartDocument();
-    xsw.writeStartElement("Orders");
-    
+	var file = new File(fullPath);
+	var fileWriter = new FileWriter(file, "UTF-8");
+	var xsw = new XMLStreamWriter(fileWriter);
+	xsw.writeStartDocument();
+	xsw.writeStartElement("Orders");
+
 	while (getOrders.hasNext()) {
 		var order = getOrders.next();
-      xsw.writeStartElement("order");
-        xsw.writeAttribute("no", order.orderNo);
-        if(order.getCustomerEmail()){
-          xsw.writeStartElement("email");
-            xsw.writeCharacters(order.getCustomerEmail());
-          xsw.writeEndElement();
-        }
-        if(order.getCustomerName()){
-          xsw.writeStartElement("customerName");
-            xsw.writeCharacters(order.getCustomerName());
-          xsw.writeEndElement();
-        }
-        if(order.getCustomerNo()){
-          xsw.writeStartElement("customerNo");
-            xsw.writeCharacters(order.getCustomerNo());
-          xsw.writeEndElement();
-        }
-          xsw.writeStartElement("total");
-          xsw.writeCharacters(order.totalGrossPrice);
-          xsw.writeEndElement();
-        xsw.writeEndElement();
-    }
-    xsw.writeEndElement();
-    xsw.writeEndDocument();
-    xsw.close();
+		if (order) {
+			xsw.writeStartElement("order");
+			xsw.writeAttribute("no", order.orderNo);
+			if (order.getCustomerEmail()) {
+				xsw.writeStartElement("email");
+				xsw.writeCharacters(order.getCustomerEmail());
+				xsw.writeEndElement();
+			}
+			if (order.getCustomerName()) {
+				xsw.writeStartElement("customerName");
+				xsw.writeCharacters(order.getCustomerName());
+				xsw.writeEndElement();
+			}
+			if (order.getCustomerNo()) {
+				xsw.writeStartElement("customerNo");
+				xsw.writeCharacters(order.getCustomerNo());
+				xsw.writeEndElement();
+			}
+			xsw.writeStartElement("total");
+			xsw.writeCharacters(order.totalGrossPrice);
+			xsw.writeEndElement();
+			xsw.writeEndElement();
+		}
+	}
+	xsw.writeEndElement();
+	xsw.writeEndDocument();
+	xsw.close();
 }
 module.exports.execute = execute;
